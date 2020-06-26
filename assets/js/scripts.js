b3bc7785a555590625cd6ae5eac58d14d81b9c08
ThiryS <thiry.simon1@gmail.com>
2020-06-26 14:09:42 +0200

    form new character

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,10 @@
-// Empty JS for your own code to be here
+(function() {
+    document.getElementById("newChar").addEventListener("click", function() {
+        
+        document.getElementById("formNewChar").classList.remove("hide");
+    });
+    document.getElementById("cancel").addEventListener("click", function() {
+        
+        document.getElementById("formNewChar").classList.add("hide");
+    });
+})();
